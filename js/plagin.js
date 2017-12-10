631731cb19579aaefb68b9a7275d6576ef368066
Othman Luqman <othman.luqman.hazim@gmail.com>
2017-12-10 11:02:45 +0300

    Add Smooth scrol

--- js/plagin.js
+++ js/plagin.js
@@ -7,15 +7,26 @@ $(function () {
           $(this).addClass('active').siblings().removeClass('active');
      });
     
+    
+    $('.navbar-nav li a').click(function(){
+       
+       $('html, body').animate({
+           
+           scrollTop: $('#'+ $(this).data('value')).offset().top 
+           
+       }, 1200); 
+        
+    });
+    
 
      $('.portfolio ul li').click(function () {
           $(this).addClass('active').siblings().removeClass('active');
          var value = $(this).attr("data-rel");
          if(value == "all"){
-             $(".images").fadeIn("1000");
+             $(".images").fadeIn();
          }else{
-             $(".images").not(value).fadeOut("1000");
-             $(".images").filter(value).fadeIn("1000");
+             $(".images").not(value).fadeOut();
+             $(".images").filter(value).fadeIn();
          }
      });
 
